5a5998cd41a7f6f9dbdf6c833c9bc71f6d8b3b33
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-04-10 11:05:51 -0700

    Update build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -5,11 +5,11 @@ echo "Syncing dependencies ..."
 mkdir "data"
 
 PIDS=""
-./sync.sh https://github.com/mTresk/android_kernel_oneplus_msm8998.git -b android-9.0 "data/kernel" "${REF}" &
+./sync.sh https://github.com/mTresk/android_kernel_oneplus_msm8998.git "data/kernel" "${REF}" &
 PIDS="${PIDS} $!"
-./sync.sh https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b master "data/gcc" &
+./sync.sh https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 "data/gcc" &
 PIDS="${PIDS} $!"
-./sync.sh https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b master "data/clang" &
+./sync.sh https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 "data/clang" &
 PIDS="${PIDS} $!"
 ./sync.sh https://github.com/Atom-X-Devs/AnyKernel3.git -b master "data/anykernel" "redflare-op5" &
 PIDS="${PIDS} $!"
