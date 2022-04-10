f272c5da7947b914a3dc6eecef366e193b65f404
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-04-10 10:49:34 -0700

    Update build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -5,13 +5,13 @@ echo "Syncing dependencies ..."
 mkdir "data"
 
 PIDS=""
-./sync.sh https://github.com/mTresk/android_kernel_oneplus_msm8998.git "data/kernel" "${REF}" &
+./sync.sh https://github.com/mTresk/android_kernel_oneplus_msm8998.git -b android-9.0 "data/kernel" "${REF}" &
 PIDS="${PIDS} $!"
-./sync.sh https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 "data/gcc" &
+./sync.sh https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b master "data/gcc" &
 PIDS="${PIDS} $!"
-./sync.sh https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 "data/clang" &
+./sync.sh https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b master "data/clang" &
 PIDS="${PIDS} $!"
-./sync.sh https://github.com/mTresk/AnyKernel2.git "data/anykernel" "redflare-op5" &
+./sync.sh https://github.com/Atom-X-Devs/AnyKernel3.git -b master "data/anykernel" "redflare-op5" &
 PIDS="${PIDS} $!"
 
 for p in $PIDS; do
