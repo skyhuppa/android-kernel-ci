60288be562582c79dbe7d556ff950ca485bed885
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-04-10 11:26:14 -0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,28 +5,28 @@ GIT="git -C ${DIR}"
 echo "Obtaining '${URL}' in '${DIR}' ..."
 
 is_head () {
-    $GIT ls-remote -q --heads --exit-code "origin" "${REF}"
+    $GIT ls-remote -q --heads --exit-code "android-9.0" "${REF}"
     return $?
 }
 
 is_tag () {
-    $GIT ls-remote -q --tags --exit-code "origin" "${REF}"
+    $GIT ls-remote -q --tags --exit-code "android-9.0" "${REF}"
     return $?
 }
 
 update () {
     if is_head; then
         echo "Found branch, using its head"
-        $GIT remote set-branches --add origin "${REF}" || exit "$?"
-        $GIT fetch origin "${REF}" --depth=1 || exit "$?"
+        $GIT remote set-branches --add android-9.0 "${REF}" || exit "$?"
+        $GIT fetch android-9.0 "${REF}" --depth=1 || exit "$?"
         SRC="origin/${REF}"
     elif is_tag; then
         echo "Found tag, using its head"
         $GIT fetch origin tag "${REF}" --depth=1 || exit "$?"
         SRC="${REF}"
     elif [ -z "${REF}" ]; then
-        echo "No ref provided, using origin head"
-        $GIT fetch origin "HEAD" --depth=1 || exit "$?"
+        echo "No ref provided, using android-9.0 head"
+        $GIT fetch android-9.0 "HEAD" --depth=1 || exit "$?"
         SRC="origin/HEAD"
     else
         echo "No such tag or branch, aborting!"
